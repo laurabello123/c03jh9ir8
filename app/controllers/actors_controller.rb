84cd49ac5b47fa45a1fa2a96814e0f7e7951b9d8
Laura Bello <Laura@Cristians-MacBook-Pro-2.local>
2018-04-03 20:47:20 -0500

    create actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,23 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+
+def create
+  actor_params = params.require(:actor).permit!
+if actor_params ["death_date"].present? && actor_params["death_place"].present?
+   actor_params.merge!({"alive"=> false})
+else
+  actor_params ["alive"] = true
+end
+  actor = Actor.new(actor_params)
+if actor.save
+ redirect_to actors_path
+  end
+end
+
+  def new
+    @actor = Actor.new
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 
   Rails.application.routes.draw do
    get "actors", to: "actors#index"
+   get "actors/new", to: "actors#new"
+   post "actors", to: "actors#create"
   end
